68877e6d66248a8341bef1a3662674161296c25d
chaudhary prakash <chaudharyprakash1992@gmail.com>
2017-02-01 12:59:36 +0530

    added admin and user lock/unlock functionality

---
+++ app/admin/user.rb
@@ -0,0 +1,45 @@
+ActiveAdmin.register User do
+  permit_params :first_name, :last_name, :email, :password, :password_confirmation
+
+  index do
+    selectable_column
+    id_column
+    column :first_name
+    column :last_name
+    column :email
+    column :current_sign_in_at
+    column :sign_in_count
+    column :created_at
+    actions do |user|
+      user.locked_at? ? (link_to 'unLock', unlock_admin_user_path(user.id), method: :put) : (link_to 'Lock', lock_admin_user_path(user.id), method: :put)
+    end
+  end
+
+  filter :email
+  filter :current_sign_in_at
+  filter :sign_in_count
+  filter :created_at
+
+  form do |f|
+    f.inputs "Admin Details" do
+      f.input :first_name
+      f.input :last_name
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
+  
+  # use for user locking 
+  member_action :lock, method: :put do
+    resource.lock_access!
+    redirect_to resource_path, notice: "Locked!"
+  end
+
+  # use for user unlocking 
+  member_action :unlock, method: :put do
+    resource.unlock_access!
+    redirect_to resource_path, notice: "Locked!"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  default_url_options :host => "http://localhost:3000/"
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root :to=>'static_pages#index'
 	devise_for :users, :controllers => { 
