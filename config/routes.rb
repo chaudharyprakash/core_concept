9e59a5192905f06be6e58ead933da912678f49d2
chaudhary prakash <chaudharyprakash1992@gmail.com>
2017-07-29 18:21:53 +0530

    Added member functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root :to=>'static_pages#index'
-	devise_for :users, :controllers => { 
+	resources :rooms
+  devise_for :users, :controllers => { 
   	:sessions => "users/sessions", 
   	:registrations => "users/registrations", 
   	:passwords => "users/passwords", 
